09495933a90daf1e713bc4ee014a44203107fa21
LEEJONGSEONG153 <home153@naver.com>
2025-12-17 10:32:44 +0900

    Observe-part2

--- src/active_menu.js
+++ src/active_menu.js
@@ -16,22 +16,35 @@ const visibleSections = sectionIds.map(() => false);
 const observer = new IntersectionObserver(
   (entries) => observerCallback(entries),
   {
-    root: null,
-    threshold: 0,
+    rootMargin: '-20% 0px 0px 0px',
+    threshold: [0, 0.98],
   }
 );
 
 sections.forEach((item) => observer.observe(item));
 
 const observerCallback = (entries) => {
+  let selectLastOne;
+
   entries.forEach((entry) => {
     const index = sectionIds.indexOf(`#${entry.target.id}`);
     visibleSections[index] = entry.isIntersecting;
-    console.log(visibleSections);
-    if (entry.isIntersecting) {
-      navItems.forEach((item) => item.classList.remove('active'));
-      const findMenu = document.querySelector(`[data-menu=${entry.target.id}]`);
-      findMenu.classList.add('active');
-    }
+    selectLastOne =
+      index === sectionIds.length - 1 &&
+      entry.isIntersecting &&
+      entry.intersectionRatio >= 0.95;
   });
+
+  const navIndex = selectLastOne
+    ? sectionIds.length - 1
+    : findFirstIntersecting(visibleSections);
+
+  navItems.forEach((item) => item.classList.remove('active'));
+  const findMenu = navItems[navIndex];
+  findMenu.classList.add('active');
+};
+
+const findFirstIntersecting = () => {
+  const index = visibleSections.indexOf(true);
+  return index >= 0 ? index : 0;
 };
